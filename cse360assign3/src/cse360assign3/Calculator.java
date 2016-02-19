df2b8d7f53f834b93a2152d977b9566199133536
CThompson <cmthom10@asu.edu>
2016-02-19 09:23:13 -0700

    Original file with some preliminary Javadoc comments.

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,63 @@
+/**
+ * @author original author
+ * @version original version 2016
+ */
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** getTotal returns the total value.
+	 * 
+	 * @return total 	the integer total.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** add currently does nothing.
+	 * 
+	 *
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/** subtract currently does nothing.
+	 * 
+	 *
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/** multiply currently does nothing.
+	 * 
+	 *
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/** divide currently does nothing.
+	 * 
+	 *@param value	an integer number.
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** getHistory currently does nothing.
+	 * 
+	 *@return an empty string.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
